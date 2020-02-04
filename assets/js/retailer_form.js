db46c683193542f0d62787897a1acea38bfe6824
brunorpassos <brurpassos@gmail.com>
2020-02-04 10:32:46 -0300

    Conditional fiels rendering

---
+++ assets/js/retailer_form.js
@@ -0,0 +1,18 @@
+ShowRelatedFields = function(){
+  function toggleFields() {
+
+  }
+
+  function showFields() {
+    console.log("noix")
+  }
+
+  function hideFields() {
+
+  }
+// private
+
+  return {
+    toggleFields: toggleFields
+  }
+}();
