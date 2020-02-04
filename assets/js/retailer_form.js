97ce3357fcef9ce938f60c96681601c54a883711
brunorpassos <brurpassos@gmail.com>
2020-02-04 11:41:26 -0300

    Toggle fields done

--- assets/js/retailer_form.js
+++ assets/js/retailer_form.js
@@ -1,16 +1,37 @@
 ShowRelatedFields = function(){
-  function toggleFields() {
+  const storeType = [null, "physical_store", "online_store", "physical_and_online", "reseller"];
 
+  function toggleFields(selectedItem) {
+    var currentStoreType = storeType[selectedItem.selectedIndex];
+
+    switch (currentStoreType) {
+      case "physical_store":
+        showField("customer-store");
+        hideField("customer-website");
+        break;
+      case "online_store":
+        showField("customer-website");
+        hideField("customer-store");
+        break;
+      case "physical_and_online":
+        showField("customer-store");
+        showField("customer-website");
+        break;
+      default :
+        hideField("customer-store");
+        hideField("customer-website");
+    }
   }
 
-  function showFields() {
-    console.log("noix")
-  }
-
-  function hideFields() {
-
-  }
 // private
+
+  function showField(fieldToShow) {
+    document.getElementById(fieldToShow).classList.remove("gnd-retailer-conditional-form-field");
+  }
+
+  function hideField(fieldToHide) {
+    document.getElementById(fieldToHide).classList.add("gnd-retailer-conditional-form-field");
+  }
 
   return {
     toggleFields: toggleFields
